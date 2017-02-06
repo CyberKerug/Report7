4738af6e04a19a940f84f5cb123e60f5dc80b3fc
Takeaki Onaga <e165735@ie.u-ryukyu.ac.jp>
2017-02-07 00:19:39 +0900

    I confused...

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        int letter,first,second;
+        int letter = 0,first = -1,second = -1;
         String input;
         Scanner in = new Scanner(System.in);
         System.out.println("ポケベル入力装置起動");
         System.out.println("文字数を入力してください。(濁点や半濁点も1文字としてカウントされます。: ");
-        letter = (int) input;
+        input = in.nextLine();
+        
 
     }
 }
