9b810c386c8ffd4339dd7006e7d6c0f51b8cd64d
CyberKerug <e165735@ie.u-ryukyu.ac.jp>
2017-02-07 00:12:25 +0900

    Create Line.java
    First number is line.
    Second Line is row.

---
+++ Line.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+
+public class Line {
+    public static void character(Character[] args) {
+        int first = -1;
+        int second = -1;
+
+        if(first == 1){
+
+        }else if(first ==2){
+
+        }else if(first ==3){
+
+        }else if(first ==4){
+
+        }else if(first ==5){
+
+        }else if(first ==6){
+
+        }else if(first ==7){
+
+        }else if(first ==8){
+
+        }else if(first ==9){
+
+        }else if(first ==0){
+
+        }else{
+            System.out.println("0から9の範囲で入力してください。");
+        }
+    }
+}
