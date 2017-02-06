46074f322c823599461675a629669349210cc331
CyberKerug <e165735@ie.u-ryukyu.ac.jp>
2017-02-07 00:13:12 +0900

    Create Main.java
    It is not complete.

---
+++ Main.java
@@ -0,0 +1,14 @@
+//ポケットベルの文字入力を再現したものである。
+//928313228313410412210423と入力すると、"りゆうきゆうた゛いか゛く"と表示される。
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int letter,first,second;
+        String input;
+        Scanner in = new Scanner(System.in);
+        System.out.println("ポケベル入力装置起動");
+        System.out.println("文字数を入力してください。(濁点や半濁点も1文字としてカウントされます。: ");
+        letter = (int) input;
+
+    }
+}
