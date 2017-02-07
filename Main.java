dad2ded79fa349410e51677bcef85c93f7cfd035
Takeaki Onaga <e165735@ie.u-ryukyu.ac.jp>
2017-02-07 09:48:16 +0900

    Input problem is solved.

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         int let = 0,letter = 0,first = -1,second = -1;
-        String input;
+        String sbox;
         Scanner in = new Scanner(System.in);
         System.out.println("ポケベル入力装置起動");
         System.out.println("文字数を入力してください。(濁点や半濁点も1文字としてカウントされます。: ");
@@ -12,13 +12,16 @@ public class Main {
 
         char[] box = new char[letter];
 
-        while (let < letter);
-        System.out.println("1つ目の数字を入力してください。: ");
-        first = in.nextInt();
-        System.out.println("2つ目の数字を入力してください。: ");
-        second = in.nextInt();
+        while (let < letter){
+            System.out.println("1つ目の数字を入力してください。: ");
+            first = in.nextInt();
+            System.out.println("2つ目の数字を入力してください。: ");
+            second = in.nextInt();
 
-
+        }
+        sbox =  toString(box[]);
+        System.out.println("あなたが入力した文字は、" + sbox + "です。");
+        System.out.println("ポケベル入力装置を終了します。");
 
     }
 }
