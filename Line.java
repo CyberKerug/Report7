b253fd4bf14202611a8e9968493e8a3f6ebf0948
Takeaki Onaga <e165735@ie.u-ryukyu.ac.jp>
2017-02-07 09:23:42 +0900

    List Box is not found...

--- Line.java
+++ Line.java
@@ -1,32 +1,36 @@
 import java.lang.*;
+import java.lang.Character;
 
 public class Line {
     public static void character(Character[] args) {
         int first = -1;
         int second = -1;
+        char le;
 
         if(first == 1){
-
+            lin = Character.ALine;
         }else if(first ==2){
-
+            lin = Character.KLine;
         }else if(first ==3){
-
+            lin = Character.SLine;
         }else if(first ==4){
-
+            lin = Character.TLine;
         }else if(first ==5){
-
+            lin = Character.NLine;
         }else if(first ==6){
-
+            lin = Character.HLine;
         }else if(first ==7){
-
+            lin = Character.MLine;
         }else if(first ==8){
-
+            lin = Character.YLine;
         }else if(first ==9){
-
+            lin = Character.RLine;
         }else if(first ==0){
-
+            lin = Character.WLine;
         }else{
             System.out.println("0から9の範囲で入力してください。");
         }
+
+         le=lin[second];
     }
 }
--- Main.java
+++ Main.java
@@ -3,13 +3,22 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        int letter = 0,first = -1,second = -1;
+        int let = 0,letter = 0,first = -1,second = -1;
         String input;
         Scanner in = new Scanner(System.in);
         System.out.println("ポケベル入力装置起動");
         System.out.println("文字数を入力してください。(濁点や半濁点も1文字としてカウントされます。: ");
-        input = in.nextLine();
-        
+        letter = in.nextInt();
+
+        char[] box = new char[letter];
+
+        while (let < letter);
+        System.out.println("1つ目の数字を入力してください。: ");
+        first = in.nextInt();
+        System.out.println("2つ目の数字を入力してください。: ");
+        second = in.nextInt();
+
+
 
     }
 }
